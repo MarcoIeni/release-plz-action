c4287116ccaf66646432facd88cc786216c4d105
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2022-04-03 14:06:32 +0200

    define inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,24 @@
 #!/bin/bash -l
 set -euo pipefail
 
-release-plz $@
+if [ "${INPUT_NO-CHANGELOG}" != "false" ]
+then
+    NO_CHANGELOG="--no-changelog"
+fi
+
+if [ -z "${INPUT_REGISTRY}"]
+then
+    ALT_REGISTRY+="--registry ${INPUT_REGISTRY}"
+fi
+
+release-plz release-pr\
+    --github-token ${GITHUB_TOKEN}\
+    --repo-url https://github.com/${GITHUB_REPOSITORY}\
+    ${NO_CHANGELOG}\
+    ${ALT_REGISTRY}\
+    ${INPUT_ARGS}
+
+
 exit_code=$?
 
 # Pass exit code to the next step

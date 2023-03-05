bb4317eecf104c12b0214d4159b823764d1e7295
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-03-05 01:37:19 +0100

    chore(updater): create release (#36)

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -1,20 +1,40 @@
+use std::process::Command;
+
 use clap::Parser;
 
 use crate::args::CliArgs;
 
 mod args;
 mod pr;
+mod release;
 
 const ACTION_YML_PATH: &str = "../action.yml";
+
+pub fn latest_release(repo: &str) -> String {
+    let release_plz_tag = Command::new("gh")
+        .args(&["release", "list", "--limit", "1", "--repo", repo])
+        .output()
+        .expect("failed to execute process");
+    let release_plz_tag = String::from_utf8(release_plz_tag.stdout).unwrap();
+    let release_plz_tag = release_plz_tag.trim();
+    println!("latest tag: {repo}: `{}`", release_plz_tag);
+    release_plz_tag
+        .split_whitespace()
+        .next()
+        .unwrap()
+        .to_string()
+}
 
 fn main() {
     let args = CliArgs::parse();
     match args.command {
         args::Command::Pr => {
-            let release_plz_tag = pr::latest_release("MarcoIeni/release-plz");
+            let release_plz_tag = latest_release("MarcoIeni/release-plz");
             pr::update_action_yml(&release_plz_tag);
             pr::create_pr(&release_plz_tag);
         }
-        args::Command::Release => todo!(),
+        args::Command::Release => {
+            release::create_release();
+        }
     }
 }
--- updater/src/pr.rs
+++ updater/src/pr.rs
@@ -1,21 +1,6 @@
 use std::process::Command;
 
-use crate::ACTION_YML_PATH;
-
-pub fn latest_release(repo: &str) -> String {
-    let release_plz_tag = Command::new("gh")
-        .args(&["release", "list", "--limit", "1", "--repo", repo])
-        .output()
-        .expect("failed to execute process");
-    let release_plz_tag = String::from_utf8(release_plz_tag.stdout).unwrap();
-    let release_plz_tag = release_plz_tag.trim();
-    println!("latest tag: {repo}: `{}`", release_plz_tag);
-    release_plz_tag
-        .split_whitespace()
-        .next()
-        .unwrap()
-        .to_string()
-}
+use crate::{latest_release, ACTION_YML_PATH};
 
 fn release_plz_line(action_yml: &str) -> Option<String> {
     for line in action_yml.lines() {

f4cbc4f821699edff369e74f9069fe735a307ec0
Marco Ieni <11428655+MarcoIeni@users.noreply.github.com>
2023-03-10 14:16:45 +0000

    chore(updater): git pull before release (#39)

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -10,9 +10,22 @@ mod release;
 
 const ACTION_YML_PATH: &str = "../action.yml";
 
+fn git_pull() {
+    let repo = git_cmd::Repo::new(".").unwrap();
+    let current_branch = repo.git(&["rev-parse", "--abbrev-ref", "HEAD"]).unwrap();
+    if current_branch != "main" {
+        panic!("You are not on the main branch");
+    }
+    let status = repo.git(&["status", "--porcelain"]).unwrap();
+    if !status.is_empty() {
+        panic!("You have uncommitted changes");
+    }
+    repo.git(&["pull"]).unwrap();
+}
+
 pub fn latest_release(repo: &str) -> String {
     let release_plz_tag = Command::new("gh")
-        .args(&["release", "list", "--limit", "1", "--repo", repo])
+        .args(["release", "list", "--limit", "1", "--repo", repo])
         .output()
         .expect("failed to execute process");
     let release_plz_tag = String::from_utf8(release_plz_tag.stdout).unwrap();
@@ -34,6 +47,7 @@ fn main() {
             pr::create_pr(&release_plz_tag);
         }
         args::Command::Release => {
+            git_pull();
             release::create_release();
         }
     }
